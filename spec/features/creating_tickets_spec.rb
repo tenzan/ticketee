1cec9b10ef3f6da5c8535b0f029a9051c9f4c5eb
Askarbek Karasaev <askar.karasaev@gmail.com>
2015-07-14 14:16:29 +0900

    removed redundant scenario

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -13,6 +13,9 @@ RSpec.feature 'Users can create new tickets' do
     fill_in "Description", with: "My pages are ugly!"
     click_button "Create Ticket"
     expect(page).to have_content "Ticket has been created."
+    within("#ticket") do
+      expect(page).to have_content "Author: #{user.email}"
+    end
   end
   scenario "when providing invalid attributes" do
     click_button "Create Ticket"
@@ -27,13 +30,5 @@ RSpec.feature 'Users can create new tickets' do
     expect(page).to have_content "Ticket has not been created."
     expect(page).to have_content "Description is too short"
   end
-  scenario "with valid attributes" do
-    fill_in "Name", with: "Non-standards compliance"
-    fill_in "Description", with: "My pages are ugly!"
-    click_button "Create Ticket"
-    expect(page).to have_content "Ticket has been created."
-    within("#ticket") do
-      expect(page).to have_content "Author: #{user.email}"
-    end
-  end
+
 end
