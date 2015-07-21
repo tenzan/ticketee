d228893e996027db41625f40f90a0fd3588f9e4d
Askarbek Karasaev <askar.karasaev@gmail.com>
2015-07-21 13:42:33 +0900

    Don't list projects that a user doesn't have permission to see

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   rescue_from Pundit::NotAuthorizedError, with: :not_authorized
 
+  def index
+  	@projects = policy_scope(Project)
+  end
+
   private
-  
-    def not_authorized
-    	redirect_to root_path, alert: "You aren't allowed to do that."
-	end
+
+  def not_authorized
+  	redirect_to root_path, alert: "You aren't allowed to do that."
+  end
 end
